c69578d2438870e6fe2a99fe74f098b9908572f8
suryaajis330 <suryaajis330@gmail.com>
2021-11-11 06:45:42 +0700

    add user testing

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -2,17 +2,22 @@ const request = require("supertest");
 const app = require("../app");
 const { User } = require("../models");
 
-let token
-let loginParams
+let token;
+let loginParams;
 beforeAll(async () => {
+  await User.create({
+    name: "user.test",
+    email: "user.test@mail.com",
+    password: "123456789",
+    role: "User",
+  });
+
   loginParams = {
     email: "user.test@mail.com",
     password: "123456789",
   };
 
-  const { body } = await request(app)
-    .post("/public/login")
-    .send(loginParams);
+  const { body } = await request(app).post("/public/login").send(loginParams);
 
   token = body.access_token;
 });
@@ -26,12 +31,13 @@ afterAll(async () => {
 });
 
 describe("GET /users", () => {
-  test("200 success get login user", (done) => {
+  test("[200 - success] get login user", (done) => {
     request(app)
       .get("/public/users")
+      .set("access_token", token)
       .then((response) => {
         const { body, status } = response;
-        console.log(response.body)
+        console.log(response.body);
         expect(status).toBe(200);
         expect(body).toEqual(expect.any(Object));
         done();
@@ -42,37 +48,46 @@ describe("GET /users", () => {
   });
 });
 
-// describe("GET /users/:userId", () => {
-//   test("200 success get courses by courseId", (done) => {
-//     request(app)
-//       .get("/public/courses/2")
-//       .then((response) => {
-//         const { body, status } = response;
-//         console.log(response.body);
-//         expect(body).toEqual(expect.any(Object));
-//         expect(body).toHaveProperty("name");
-//         expect(body).toHaveProperty("Category");
-//         expect(body).toHaveProperty("Videos");
-//         expect(status).toBe(200);
-//         done();
-//       })
-//       .catch((err) => {
-//         done(err);
-//       });
-//   });
+describe("PUT /public/users", () => {
+  test("[200 - success] update public login user", (done) => {
+    const dataUpdate = {
+      name: "user.update",
+      email: "user.update@mail.com"
+    }
 
-//   test("[404 - Course Not Found] get course with invaild course id ", (done) => {
-//     request(app)
-//       .get("/public/courses/1000")
-//       .then((response) => {
-//         const { status, body } = response;
-//         expect(status).toBe(404);
-//         expect(body).toEqual(expect.any(Object));
-//         expect(body).toHaveProperty("message", "Course not found");
-//         done();
-//       })
-//       .catch((err) => {
-//         done(err);
-//       });
-//   });
-// });
+    request(app)
+      .put("/public/users")
+      .set("access_token", token)
+      .send(dataUpdate)
+      .then((response) => {
+        const { body, status } = response;
+        expect(body).toEqual(expect.any(Object));
+        expect(status).toBe(200);
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test("[401 - empty input] update public login user with empty name", (done) => {
+    const dataUpdate = {
+      name: "",
+      email: "user.update@mail.com"
+    }
+
+    request(app)
+      .put("/public/users")
+      .set("access_token", token)
+      .send(dataUpdate)
+      .then((response) => {
+        const { body, status } = response;
+        expect(body).toEqual(expect.any(Object));
+        expect(status).toBe(401);
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+});
