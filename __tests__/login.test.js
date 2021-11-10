fa062a54ae2a1fdf18781bc7b351a9f511ba7e15
suryaajis330 <suryaajis330@gmail.com>
2021-11-10 07:10:36 +0700

    repair login testing and update readme

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -7,44 +7,45 @@ beforeAll(() => {
   User.create({
     name: "user10",
     email: "user10@mail.com",
-    password: "rahasia",
+    password: "123456789",
     role: "User",
   });
 
   loginParams = {
     email: "user10@mail.com",
-    password: "rahasia",
+    password: "123456789",
   };
 });
 
-describe("[success] POST /customers/login", () => {
-  test("Menghasilkan status code 200 dan berhasil login", async () => {
-    const response = await request(app).post("/customers/login").send(loginParams);
+describe("[success] POST /public/login", () => {
+  test("Result status code 200 and success login", async () => {
+    const response = await request(app).post("/public/login").send(loginParams);
 
+    console.log(response.body)
     expect(response.status).toBe(200);
     expect(response.body).toEqual(expect.any(Object));
   });
 });
 
-describe("[failed] POST /customers/login", () => {
+describe("[failed] POST /public/login", () => {
   test("Memberikan password yang salah", async () => {
     loginParams.password = "a";
 
-    const response = await request(app).post("/customers/login").send(loginParams);
+    const response = await request(app).post("/public/login").send(loginParams);
 
     expect(response.status).toBe(401);
-    expect(response.body).toHaveProperty("message", "Invalid Email/Password");
+    expect(response.body).toHaveProperty("message", "Invalid email/password");
 
-    loginParams.password = "rahasia";
+    loginParams.password = "123456789";
   });
 
   test("Memberikan email yang salah / tidak ada di database", async () => {
     loginParams.email = "user1000@mail.com";
 
-    const response = await request(app).post("/customers/login").send(loginParams);
+    const response = await request(app).post("/public/login").send(loginParams);
 
     expect(response.status).toBe(401);
-    expect(response.body).toHaveProperty("message", "Invalid Email/Password");
+    expect(response.body).toHaveProperty("message", "Invalid email/password");
   });
 });
 
