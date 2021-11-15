58388114058102927e13a0fdd6c3d464ecdc91dc
suryaajis330 <suryaajis330@gmail.com>
2021-11-15 13:26:10 +0700

    update testing

--- __tests__/other.test.js
+++ __tests__/other.test.js
@@ -136,70 +136,44 @@ describe("POST /admin/courses", () => {
     expect(body).toHaveProperty("message", "File Format Should Be MP4");
   });
 
-  // test("[400 - Invalid Format] Catch invalid format size add course", async () => {
+  test("[400 - Invalid Format] Catch invalid format size add course", async () => {
+    
+    jest.spyOn(Course, "create").mockRejectedValue("InvalidFileSize");
+    const inputSample = {
+      name: "Bahasa korea",
+      description: "belajar cepat bahasa korea",
+      price: 112000,
+      thumbnailUrl:
+        "https://i.ytimg.com/vi/hgvZeHkFg9E/hqdefault.jpg?s…QCAokN4AQ==&rs=AOn4CLBNFG6WY9Pv5MdeSeSr5XU_k-YE_Q",
+      difficulty: "hard",
+      status: "active",
+      CategoryId: 1,
+    };
+    
+    const filePath = "assets/viral.mp4";
+    const buffer = Buffer.from(filePath)
 
-  //   const asyncMock = await jest.fn().mockRejectedValue(new Error('InvalidFileSize'));
-
-  //   const inputSample = {
-  //         name: "Bahasa korea",
-  //         description: "belajar cepat bahasa korea",
-  //         price: 112000,
-  //         thumbnailUrl:
-  //           "https://i.ytimg.com/vi/hgvZeHkFg9E/hqdefault.jpg?s…QCAokN4AQ==&rs=AOn4CLBNFG6WY9Pv5MdeSeSr5XU_k-YE_Q",
-  //         difficulty: "hard",
-  //         status: "active",
-  //         CategoryId: 1,
-  //       };
-
-  //   console.log(asyncMock())
-  //   return request(app)
-  //     .post("/admin/courses")
-  //     .set("access_token", token)
-  //     .then((response) => {
-  //       const { body, status } = response;
-  //       expect(status).toBe(500);
-  //       expect(body).toEqual(expect.any(Object));
-  //       expect(body).toHaveProperty("message", "File Size Should Not Exceeded 25MB");
-  //     })
-  // });
-
-  // test("[400 - Invalid Format] add video with invalid size ", async () => {
-  //   const inputSample = {
-  //     name: "Bahasa korea",
-  //     description: "belajar cepat bahasa korea",
-  //     price: 112000,
-  //     thumbnailUrl:
-  //       "https://i.ytimg.com/vi/hgvZeHkFg9E/hqdefault.jpg?s…QCAokN4AQ==&rs=AOn4CLBNFG6WY9Pv5MdeSeSr5XU_k-YE_Q",
-  //     difficulty: "hard",
-  //     status: "active",
-  //     CategoryId: 1,
-  //   };
-
-  //   const filePath = "assets/music.mp4";
-  //   const buffer = Buffer.from(filePath)
-
-  //   await request(app).delete("/admin/courses/1").set("access_token", token);
-
-  //   const { status, body } = await request(app)
-  //     .post("/admin/courses")
-  //     .set("access_token", token)
-  //     .field("name", inputSample.name)
-  //     .field("description", inputSample.description)
-  //     .field("price", inputSample.price)
-  //     .field("thumbnailUrl", inputSample.thumbnailUrl)
-  //     .field("difficulty", inputSample.difficulty)
-  //     .field("status", inputSample.status)
-  //     .field("CategoryId", inputSample.CategoryId)
-  //     .attach("Videos", buffer, "draw.mp4");
-
-  //   console.log(body);
-  //   expect(status).toBe(400);
-  //   expect(body).toEqual(expect.any(Object));
-  //   expect(body).toHaveProperty(
-  //     "message",
-  //     "File Size Should Not Exceeded 25MB"
-  //   );
-  // });
+    return request(app)
+      .post("/admin/courses")
+      .set("access_token", token)
+      .field("name", inputSample.name)
+      .field("description", inputSample.description)
+      .field("price", inputSample.price)
+      .field("thumbnailUrl", inputSample.thumbnailUrl)
+      .field("difficulty", inputSample.difficulty)
+      .field("status", inputSample.status)
+      .field("CategoryId", inputSample.CategoryId)
+      .attach("Videos", buffer, "draw.mp4")
+      .then((response) => {
+        const { body, status } = response;
+        expect(status).toBe(500);
+        expect(body).toEqual(expect.any(Object));
+        expect(body).toHaveProperty(
+          "message",
+          "Internal server error"
+        );
+      });
+  });
 });
 
 describe("POST /admin/videos/:courseId", () => {
@@ -207,25 +181,25 @@ describe("POST /admin/videos/:courseId", () => {
     const filePath = "assets/viral.mp4";
     const buffer = Buffer.from(filePath);
 
-    const { status, body} = await request(app)
+    const { status, body } = await request(app)
       .post("/admin/videos/1")
       .set("access_token", token)
       .attach("Videos", buffer, "viral.mp4");
 
-    expect(status).toBe(201)
-    expect(body).toEqual(expect.any(Object))
-    expect(body).toHaveProperty("name")
-    expect(body).toHaveProperty("CourseId")
+    expect(status).toBe(201);
+    expect(body).toEqual(expect.any(Object));
+    expect(body).toHaveProperty("name");
+    expect(body).toHaveProperty("CourseId");
   });
 
   test("[201 - Success] delete video by video id", async () => {
-    const { status, body} = await request(app)
+    const { status, body } = await request(app)
       .delete("/admin/videos/1")
-      .set("access_token", token)
+      .set("access_token", token);
 
-    expect(status).toBe(200)
-    expect(body).toEqual(expect.any(Object))
-    expect(body).toHaveProperty("message", "Video has been deleted")
+    expect(status).toBe(200);
+    expect(body).toEqual(expect.any(Object));
+    expect(body).toHaveProperty("message", "Video has been deleted");
   });
 
   test("[404 - Course Not Found] add video with invalid course id", async () => {
@@ -235,5 +209,15 @@ describe("POST /admin/videos/:courseId", () => {
 
     expect(status).toBe(404);
     expect(body).toEqual(expect.any(Object));
+  });
+
+  test("[404 - Video Not Found] delete video with invalid video id", async () => {
+    const { body, status } = await request(app)
+      .delete("/admin/videos/9999999")
+      .set("access_token", token);
+
+    expect(status).toBe(404);
+    expect(body).toEqual(expect.any(Object));
+    expect(body).toHaveProperty("message", "Video Not Found");
   });
 });
